661579f259b6f6b55a53bd13f552504f6df50102
Adam Fister <arfister@gmail.com>
2019-03-16 18:22:10 -0400

    implemented clear completed button functionality

--- web/js/main.js
+++ web/js/main.js
@@ -13,28 +13,31 @@ var app = new Vue({
             this.total++;
             this.newTask = '';
         },
+
         completeAll() {
-            for (var i =0; i < this.todos.length; i++) {
-                this.todos[i].completed = true;
-            };
+            this.todos.map(todo => todo.completed = true);
         },
+
         activeAgain() {
-            for (var i =0; i < this.todos.length; i++) {
-                this.todos[i].completed = false;
-            };
+            this.todos.map(todo => todo.completed = false);
         },
+
         clearCompleted() {
-            for (var i =0; i < this.todos.length; i++) {
-                if (this.todos[i].completed == true) {
+
+            for (var i = this.todos.length - 1; i >= 0; i--) {
+                if (this.todos[i].completed == true) { 
                     this.todos.splice(i, 1);
                 }
             }
         }
+
+
     },
     computed: {
         countList() {
-            return this.todos.filter((todo) => todo.completed == false)
+            return this.todos.filter(todo => todo.completed == false);
         }
+
     }
 
 })
