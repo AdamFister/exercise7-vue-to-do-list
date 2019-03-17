b7b48dfedd496273ee3d3890290b518621c779ad
Adam Fister <arfister@gmail.com>
2019-03-17 15:19:21 -0400

    fulfilled requirements

--- web/js/main.js
+++ web/js/main.js
@@ -1,51 +1,74 @@
 var app = new Vue({
     el: '#app',
     data: {
-        title: 'todos',
+        title: 'To Do',
         todos: [],
         newTask: '',
         total: 0,
-        buttonNum: 0
+        buttonNum: 0,
+        buttonMessage: 'Showing: ALL'
     },
     methods: {
         addTask() {
             this.todos.push({ text: this.newTask, completed: false, index: this.total });
             this.total++;
             this.newTask = '';
-            console.log(this.todos[this.total - 1].index);
+
+            // reassigns newly created array index values
+            for (var i = 0; i < this.todos.length; i++) {
+                this.todos[i].index = i;
+            }
         },
 
         completeAll() {
-            this.todos.map(todo => todo.completed = true);
+            for (var i = 0; i < this.todos.length; i++) {
+                this.todos[i].completed = true;
+            }
         },
 
         activeAgain() {
-            this.todos.map(todo => todo.completed = false);
+            for (var i = 0; i < this.todos.length; i++) {
+                this.todos[i].completed = false;
+            }
         },
 
         removeElement(num) {
-            //console.log("remove " + num);
-            this.todos.splice(num,1);
+            this.todos.splice(num, 1);
+
+            // reassigns newly created array index values
             for (var i = 0; i < this.todos.length; i++) {
-                // console.log("item at " + i);
-                // console.log("old index" + this.todos[i].index);
                 this.todos[i].index = i;
-                // console.log("new index" + this.todos[i].index);
             }
         },
 
         clearCompleted() {
-
+            //clears to do elements with .completed value of true
             for (var i = this.todos.length - 1; i >= 0; i--) {
-                if (this.todos[i].completed == true) { 
+                if (this.todos[i].completed == true) {
                     this.todos.splice(i, 1);
                 }
             }
+        },
+
+        setButtonNum0() {
+            this.buttonNum = 0;
+            this.buttonMessage = 'Showing: ALL'
+        },
+
+        setButtonNum1() {
+            this.buttonNum = 1;
+            this.buttonMessage = 'Showing: TO DO'
+        },
+
+        setButtonNum2() {
+            this.buttonNum = 2;
+            this.buttonMessage = 'Showing: COMPLETED'
         }
 
 
     },
     computed: {
+        //dynamically count number of to do elements
         countList() {
             return this.todos.filter(todo => todo.completed == false);
         }
